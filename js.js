af29a1790a72bee37376893ca073bb81a266b9f5
tomoda-saya <83710181+tomoda-saya@users.noreply.github.com>
2021-08-06 15:49:04 +0900

    Add files via upload

--- js.js
+++ js.js
@@ -1,87 +1,3 @@
-particlesJS("particles-js",{
-	"particles":{
-		"number":{
-			"value":24,//この数値を変更すると幾何学模様の数が増減できる
-			"density":{
-				"enable":true,
-				"value_area":800
-			}
-		},
-		"color":{
-			"value":"#73C1FF"//色
-		},
-		"shape":{
-			"type":"polygon",//形状はpolygonを指定
-			"stroke":{
-				"width":0,
-			},
-	"polygon":{
-		"nb_sides":10//多角形の角の数
-	},
-	"image":{
-		"width":190,
-		"height":100
-	}
-	},
-		"opacity":{
-		"value":0.664994832269074,
-		"random":false,
-		"anim":{
-			"enable":true,
-			"speed":2.2722661797524872,
-			"opacity_min":0.08115236356258881,
-			"sync":false
-		}
-		},
-		"size":{
-			"value":3,
-			"random":true,
-			"anim":{
-				"enable":false,
-				"speed":40,
-				"size_min":0.1,
-				"sync":false
-			}
-		},
-		"line_linked":{
-			"enable":true,
-			"distance":150,
-			"color":"#73C1FF",
-			"opacity":0.6,
-			"width":1
-		},
-		"move":{
-			"enable":true,
-			"speed":5,//この数値を小さくするとゆっくりな動きになる
-			"direction":"none",//方向指定なし
-			"random":false,//動きはランダムにしない
-			"straight":false,//動きをとどめない
-			"out_mode":"out",//画面の外に出るように描写
-			"bounce":false,//跳ね返りなし
-			"attract":{
-				"enable":false,
-				"rotateX":600,
-				"rotateY":961.4383117143238
-			}
-		}
-	},
-	"interactivity":{
-		"detect_on":"canvas",
-		"events":{
-			"onhover":{
-				"enable":false,
-				"mode":"repulse"
-			},
-	"onclick":{
-		"enable":false
-	},
-	"resize":true
-		}
-	},
-	"retina_detect":true
-});
-
-
 
 $('#container').scrollgress({//バーの高さの基準となるエリア指定
 	height: '5px',//バーの高さ
@@ -115,3 +31,123 @@ $('.slider').slick({
 	}
 ]
 });
+
+
+
+particlesJS('particles-js',
+  {
+    "particles": {
+      "number": {
+        "value": 28,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#73C1FF"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#73C1FF",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 5,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 1000,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+);
