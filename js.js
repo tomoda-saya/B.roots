cc4b6b194cfec9c614ef756a85817c9e5b69b0b5
it-work <83710181+tomoda-saya@users.noreply.github.com>
2021-08-10 03:11:43 +0900

    Add files via upload

--- js.js
+++ js.js
@@ -14,7 +14,6 @@ $('.slider').slick({
 	slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
 	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
 	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-	dots: true,//下部ドットナビゲーションの表示
 	responsive: [
 		{
 		breakpoint: 769,//モニターの横幅が769px以下の見せ方
